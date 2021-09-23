bea7e5f28fa4fc70ac9b2e13b6663a4bac619fc0
Uros Ristanovic <urosristanovic@gmail.com>
2021-09-23 22:18:05 +0200

    Finished first exercise, new contact form

---
+++ 01-new-contact/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function createMessage(value) {
+  const div = document.createElement('div');
+  const p = document.createElement('p');
+  p.textContent = value;
+  div.appendChild(p);
+  return div;
+}
+
+const saveBtn = document.getElementById('btn-submit');
+
+saveBtn.addEventListener('click', e => {
+  e.preventDefault();
+  const message = document.getElementById('message');
+
+  const paragraph = document.createElement('p');
+  const text = document.createTextNode('You successfully created new contact!');
+  paragraph.appendChild(text);
+  message.appendChild(paragraph);
+});
