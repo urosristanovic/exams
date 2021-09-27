27df9c71c008b13fb973b6dfe3d9f3767df605b3
Uros Ristanovic <urosristanovic@gmail.com>
2021-09-27 10:48:42 +0200

    Fixed validation and type text for input

--- 03-leap-year/main.js
+++ 03-leap-year/main.js
@@ -6,10 +6,11 @@ const check = document.getElementById('form');
 
 check.addEventListener('submit', e => {
   e.preventDefault();
-  const year = document.getElementById('leap_year');
-  const isLeap = isLeapYear(year.value);
+  const year = document.getElementById('leap_year').value;
+  const isLeap = isLeapYear(year);
   const result = document.getElementById('result');
-  result.innerHTML = `Year ${year.value} <strong>is${
+
+  result.innerHTML = `Year ${year} <strong>is${
     isLeap ? '' : ' not'
   }</strong> leap year.`;
 });
