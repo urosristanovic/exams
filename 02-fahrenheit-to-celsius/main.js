5eccd48418d59c448092f1ca0042f1e23b2d14ef
Uros Ristanovic <urosristanovic@gmail.com>
2021-09-27 10:38:50 +0200

    Added form and validation, fixed style

--- 02-fahrenheit-to-celsius/main.js
+++ 02-fahrenheit-to-celsius/main.js
@@ -1,39 +1,16 @@
 function calculateCelsius(degree) {
-  return (degree - 32) * (5 / 9);
+  return (degree - 32) * (5 / 9).toFixed(2);
 }
 
-const convert = document.getElementById('convert');
+const convert = document.getElementById('form');
 
-convert.addEventListener('click', () => {
+convert.addEventListener('submit', e => {
+  e.preventDefault();
   const fahrenheit = document.getElementById('fahrenheit');
   const celsius = document.getElementById('celsius');
   const calculation = document.getElementById('calculation');
+
   celsius.value = calculateCelsius(fahrenheit.value);
+
   calculation.innerText = `(${fahrenheit.value}°F - 32) × 5/9`;
 });
-
-// read only celsius
-
-// const convert = document.getElementById('convert');
-
-// convert.addEventListener('click', () => {
-//   getFahrenheit();
-// });
-
-// function getFahrenheit() {
-//   const fahrenheit = document.getElementById('fahrenheit');
-
-//   console.log(fahrenheit.value);
-
-//   fahrenheit.addEventListener('change', e => {
-//     const fahValue = e.target.value;
-//     calculateCelsius(fahValue);
-//   });
-// }
-
-// function calculateCelsius(value) {
-//   const celsius = document.getElementById('celsius');
-//   const calculation = document.getElementById('calculation');
-//   celsius.value = (value - 32) * (5 / 9);
-//   calculation.innerText = `(${value}°F - 32) × 5/9`;
-// }
