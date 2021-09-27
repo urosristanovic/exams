3a078fca032ee8ea3ad9a91fe3c870f50fec86a0
Uros Ristanovic <urosristanovic@gmail.com>
2021-09-27 23:43:38 +0200

    Added list of guesses

--- 08-guess-game/main.js
+++ 08-guess-game/main.js
@@ -1,14 +1,16 @@
-let randomNumber = setRandomNumber();
-let live = 5;
+setRandomNumber();
 
 const form = document.getElementById('form-guess');
 
 form.addEventListener('submit', e => {
   e.preventDefault();
   const guess = Number(document.getElementById('guess').value);
+  const randomNumber = document.getElementById('random').value;
   const isValidGuess = isValid(guess);
+
   if (isValidGuess) {
-    checkNumber(guess);
+    guessesList(guess);
+    checkNumber(guess, randomNumber);
   }
 });
 
@@ -23,16 +25,35 @@ function isValid(number) {
 
   return !isValid;
 }
+function guessesList(guess) {
+  const list = document.getElementsByTagName('li');
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].innerText != guess) {
+      console.log('damn');
+    }
+  }
+  const ol = document.querySelector('ol');
+  const li = createListItem(guess);
+  li.setAttribute('id', 'list-item');
+  ol.appendChild(li);
+}
+function createListItem(value) {
+  const li = document.createElement('li');
+  const em = document.createElement('em');
+  em.textContent = value;
+  li.appendChild(em);
+  return li;
+}
 function setRedClass(addRed, removeRed) {
   addRed.classList.add('red');
   removeRed.classList.remove('red');
 }
 function setRandomNumber() {
   const number = Math.floor(Math.random() * 100) + 1;
+  document.getElementById('random').value = number;
   console.log(number);
-  return number;
 }
-function checkNumber(number) {
+function checkNumber(number, randomNumber) {
   const below = document.getElementById('below');
   const higher = document.getElementById('higher');
   const correct = document.getElementById('correct');
@@ -57,9 +78,10 @@ function checkNumber(number) {
 
 function countLives() {
   const game = document.getElementById('game-result');
-  const haveMoreLives = live > 1;
+  const lives = document.getElementById('counter').value;
+  const haveMoreLives = lives > 1;
 
-  decrementLives();
+  decrementLives(lives);
   if (!haveMoreLives) {
     document.getElementById('try').disabled = true;
     game.classList.remove('green');
@@ -69,15 +91,15 @@ function countLives() {
   }
 }
 
-function decrementLives() {
+function decrementLives(lives) {
   const reds = document.getElementsByClassName('red');
-  reds[live].classList.remove('red');
-  live--;
+  reds[lives].classList.remove('red');
+  document.getElementById('counter').value--;
 }
 
 function playAgain() {
-  const div = document.createElement('div');
   const hint = document.getElementById('play-again');
+  const div = document.createElement('div');
   div.setAttribute('id', 'play-container');
   div.innerHTML = `<h4>Do you want to play again?</h4>
   <button id="yes">Yes</button>
@@ -97,26 +119,29 @@ function setGame() {
   const higher = document.getElementById('higher');
   const correct = document.getElementById('correct');
 
-  const reds = document.getElementsByClassName('fa-heart');
+  const hearts = document.getElementsByClassName('fa-heart');
 
   yes.addEventListener('click', () => {
-    randomNumber = setRandomNumber();
-    live = 5;
     document.getElementById('guess').value = '';
+    document.getElementById('counter').value = 5;
     document.getElementById('game-result').innerText = '';
     document.getElementById('try').disabled = false;
+    document.getElementById('list-guesses').innerHTML = '';
+
+    setRandomNumber();
 
     higher.classList.remove('red');
     below.classList.remove('red');
     correct.classList.remove('green');
     playContainer.remove();
 
-    for (let i = 0; i < reds.length; i++) {
-      reds[i].classList.add('red');
+    for (let i = 0; i < hearts.length; i++) {
+      hearts[i].classList.add('red');
     }
   });
 
   no.addEventListener('click', () => {
+    document.getElementById('list-guesses').innerHTML = '';
     guessContainer.innerHTML = `<div class="thank-you green">
     <h3>Bye, hope you had fun! :)</h3></div>`;
     gameContainer.remove();
