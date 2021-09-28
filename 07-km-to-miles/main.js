35548c01562596dc6dc204eb31b6770dd7efa79a
Uros Ristanovic <urosristanovic@gmail.com>
2021-09-28 15:24:52 +0200

    Fixed km to miles messages

--- 07-km-to-miles/main.js
+++ 07-km-to-miles/main.js
@@ -4,30 +4,22 @@ function convertKmToMiles(kilometers) {
 }
 
 function createMessage(km, miles) {
-  const p = document.createElement('p');
-  p.classList.add('green');
-  p.innerHTML = `Distance of <strong>${km}</strong> km is <strong>${miles}</strong> miles.`;
-  return p;
+  const message = document.getElementById('message');
+  message.innerHTML = `Distance of <strong>${km}</strong> km is <strong>${miles}</strong> miles.`;
 }
 
 const convert = document.getElementById('form');
 convert.addEventListener('submit', e => {
   e.preventDefault();
-  const heading = document.getElementById('miles');
-  const negative = document.getElementById('negative');
-  const kilometers = document.getElementById('kilometers');
-  const km = kilometers.value;
-  if (km < 0) {
-    negative.classList.add('red');
-    negative.innerHTML = `Please use positive numbers for conversion kilometers in miles.`;
-  } else {
-    negative.classList.remove('red');
-    negative.innerHTML = ``;
-    const miles = convertKmToMiles(km).toFixed(2);
-    const mes = createMessage(km, miles);
-    heading.appendChild(mes);
-  }
-  kilometers.value = '';
-});
+  const km = document.getElementById('kilometers').value;
 
-// validate kilometers
+  if (km < 0) {
+    document.getElementById('negative').style.display = 'block';
+    document.getElementById('message').style.display = 'none';
+  } else {
+    document.getElementById('negative').style.display = 'none';
+    document.getElementById('message').style.display = 'block';
+    const miles = convertKmToMiles(km).toFixed(2);
+    createMessage(km, miles);
+  }
+});
