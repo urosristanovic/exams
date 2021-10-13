6507a729d8863deaeef6bf8d20f34b6e8b778a90
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-13 22:05:04 +0200

    Implemented multi language options and fixed design

--- 01-new-contact/message.js
+++ 01-new-contact/message.js
@@ -1,15 +1,50 @@
 const params = new URLSearchParams(location.search);
 
 const firstName = params.get('name');
-const surename = params.get('surename');
+const surname = params.get('surname');
 const date = params.get('date');
 
-const message = document.getElementById('message');
-const contactMessage = createMessage(firstName, surename, date);
-message.appendChild(contactMessage);
+function createMessage(name, surname, date) {
+  const fullName = document.getElementById('full-name');
+  const dobMsg = document.getElementById('em-dob');
 
-function createMessage(name, surename, date) {
-  const p = document.createElement('p');
-  p.innerHTML = `Date of birth: <em>${date}</em>. <br> New contact <strong>${name} ${surename}</strong> successfully created!`;
-  return p;
+  fullName.innerText = `${name} ${surname}`;
+  dobMsg.innerText = `${date}.`;
+
+  checkLanguage();
+}
+
+createMessage(firstName, surname, date);
+
+function checkLanguage(language) {
+  if (language === 'sr') {
+    translateToSerbian();
+  } else {
+    translateToEnglish();
+  }
+}
+
+function getLabels() {
+  const newContact = document.getElementById('new-contact');
+  const message = document.getElementById('success');
+  const dob = document.getElementById('dob');
+
+  return {
+    newContact,
+    message,
+    dob,
+  };
+}
+
+function translateToEnglish() {
+  const labels = getLabels();
+  labels.newContact.innerText = `New contact: `;
+  labels.message.innerText = `Has successfully created!`;
+  labels.dob.innerText = `Date of birth: `;
+}
+function translateToSerbian() {
+  const labels = getLabels();
+  labels.newContact.innerText = `Nov kontakt: `;
+  labels.message.innerText = `Uspešno kreiran!`;
+  labels.dob.innerText = `Datum rođenja: `;
 }
