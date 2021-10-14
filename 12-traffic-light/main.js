400f51609ff93e027fcc742416643dce4b2d26d5
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-14 23:22:35 +0200

    Cleaned comments

--- 12-traffic-light/main.js
+++ 12-traffic-light/main.js
@@ -5,32 +5,25 @@ const right = document.getElementById('right-arrow');
 
 function trafficLight() {
   setTimeout(() => {
-    // green.style.opacity = 1;
     green.classList.add('opacity-1');
   }, 1000);
   setTimeout(() => {
     green.classList.remove('opacity-1');
     green.classList.add('opacity-03');
-    // green.style.opacity = 0.3;
   }, 2000);
   setTimeout(() => {
-    // green.style.opacity = 1;
     green.classList.remove('opacity-03');
     green.classList.add('opacity-1');
   }, 3000);
   setTimeout(() => {
-    // green.style.opacity = 0.3;
     green.classList.remove('opacity-1');
     green.classList.add('opacity-03');
   }, 4000);
   setTimeout(() => {
-    // green.style.opacity = 1;
     green.classList.remove('opacity-03');
     green.classList.add('opacity-1');
   }, 5000);
   setTimeout(() => {
-    // green.style.opacity = 0.3;
-    // yellow.style.opacity = 1;
     green.classList.remove('opacity-1');
     green.classList.add('opacity-03');
     yellow.classList.add('opacity-1');
@@ -40,13 +33,9 @@ function trafficLight() {
     yellow.classList.add('opacity-03');
     red.classList.add('opacity-1');
     right.classList.add('opacity-1');
-    // yellow.style.opacity = 0.3;
-    // red.style.opacity = 1;
-    // right.style.opacity = 1;
   }, 8000);
   setTimeout(() => {
     yellow.classList.add('opacity-1');
-    // yellow.style.opacity = 1;
   }, 12000);
   setTimeout(() => {
     yellow.classList.remove('opacity-1');
@@ -57,10 +46,6 @@ function trafficLight() {
     right.classList.add('opacity-02');
     green.classList.remove('opacity-03');
     green.classList.add('opacity-1');
-    // yellow.style.opacity = 0.3;
-    // red.style.opacity = 0.3;
-    // right.style.opacity = 0.2;
-    // green.style.opacity = 1;
   }, 14000);
 }
 
