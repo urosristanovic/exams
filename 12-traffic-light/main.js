7904e087f2d7156e2214f4c1f1b25ce002b7d21a
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-14 23:21:52 +0200

    Implemented arrow to right

--- 12-traffic-light/main.js
+++ 12-traffic-light/main.js
@@ -1,38 +1,66 @@
 const red = document.getElementById('red');
 const yellow = document.getElementById('yellow');
 const green = document.getElementById('green');
+const right = document.getElementById('right-arrow');
 
 function trafficLight() {
   setTimeout(() => {
-    green.style.opacity = 1;
+    // green.style.opacity = 1;
+    green.classList.add('opacity-1');
   }, 1000);
   setTimeout(() => {
-    green.style.opacity = 0.3;
+    green.classList.remove('opacity-1');
+    green.classList.add('opacity-03');
+    // green.style.opacity = 0.3;
   }, 2000);
   setTimeout(() => {
-    green.style.opacity = 1;
+    // green.style.opacity = 1;
+    green.classList.remove('opacity-03');
+    green.classList.add('opacity-1');
   }, 3000);
   setTimeout(() => {
-    green.style.opacity = 0.3;
+    // green.style.opacity = 0.3;
+    green.classList.remove('opacity-1');
+    green.classList.add('opacity-03');
   }, 4000);
   setTimeout(() => {
-    green.style.opacity = 1;
+    // green.style.opacity = 1;
+    green.classList.remove('opacity-03');
+    green.classList.add('opacity-1');
   }, 5000);
   setTimeout(() => {
-    green.style.opacity = 0.3;
-    yellow.style.opacity = 1;
+    // green.style.opacity = 0.3;
+    // yellow.style.opacity = 1;
+    green.classList.remove('opacity-1');
+    green.classList.add('opacity-03');
+    yellow.classList.add('opacity-1');
   }, 6000);
   setTimeout(() => {
-    yellow.style.opacity = 0.3;
-    red.style.opacity = 1;
+    yellow.classList.remove('opacity-1');
+    yellow.classList.add('opacity-03');
+    red.classList.add('opacity-1');
+    right.classList.add('opacity-1');
+    // yellow.style.opacity = 0.3;
+    // red.style.opacity = 1;
+    // right.style.opacity = 1;
   }, 8000);
   setTimeout(() => {
-    yellow.style.opacity = 1;
+    yellow.classList.add('opacity-1');
+    // yellow.style.opacity = 1;
   }, 12000);
   setTimeout(() => {
-    yellow.style.opacity = 0.3;
-    red.style.opacity = 0.3;
-    green.style.opacity = 1;
+    yellow.classList.remove('opacity-1');
+    yellow.classList.add('opacity-03');
+    red.classList.remove('opacity-1');
+    red.classList.add('opacity-03');
+    right.classList.remove('opacity-1');
+    right.classList.add('opacity-02');
+    green.classList.remove('opacity-03');
+    green.classList.add('opacity-1');
+    // yellow.style.opacity = 0.3;
+    // red.style.opacity = 0.3;
+    // right.style.opacity = 0.2;
+    // green.style.opacity = 1;
   }, 14000);
 }
 
