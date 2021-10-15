245aed568c25a290ddd02932c3ced24eccaf51d9
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-15 10:05:57 +0200

    Added style for lights (scale)

--- 12-traffic-light/main.js
+++ 12-traffic-light/main.js
@@ -54,3 +54,39 @@ trafficLight();
 setInterval(() => {
   trafficLight();
 }, 15000);
+
+// function trafficLight() {
+//   setTimeout(() => {
+//     green.style.opacity = 1;
+//   }, 1000);
+//   setTimeout(() => {
+//     green.style.opacity = 0.3;
+//   }, 2000);
+//   setTimeout(() => {
+//     green.style.opacity = 1;
+//   }, 3000);
+//   setTimeout(() => {
+//     green.style.opacity = 0.3;
+//   }, 4000);
+//   setTimeout(() => {
+//     green.style.opacity = 1;
+//   }, 5000);
+//   setTimeout(() => {
+//     green.style.opacity = 0.3;
+//     yellow.style.opacity = 1;
+//   }, 6000);
+//   setTimeout(() => {
+//     yellow.style.opacity = 0.3;
+//     red.style.opacity = 1;
+//     right.style.opacity = 1;
+//   }, 8000);
+//   setTimeout(() => {
+//     yellow.style.opacity = 1;
+//   }, 12000);
+//   setTimeout(() => {
+//     yellow.style.opacity = 0.3;
+//     red.style.opacity = 0.3;
+//     right.style.opacity = 0.2;
+//     green.style.opacity = 1;
+//   }, 14000);
+// }
