650f624d1ce7e9b4308e69e37b7ea42f2c89feb4
Uros Ristanovic <urosristanovic@gmail.com>
2021-10-15 10:04:38 +0200

    Fixed stowatch to work with hundreds

--- 11-stopwatch/main.js
+++ 11-stopwatch/main.js
@@ -3,45 +3,33 @@ const miliseconds = document.getElementById('miliseconds');
 
 let time;
 
-let ms = 0;
-let sec = 0;
-let min = 0;
-let hr = 0;
+let hundreds = 0;
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
 
 function stopwatch() {
-  sec = parseInt(sec);
-  min = parseInt(min);
-  hr = parseInt(hr);
-
-  ms++;
-  if (ms == 10) {
-    ms = 0;
-    sec++;
+  hundreds++;
+  if (hundreds == 100) {
+    hundreds = 0;
+    seconds++;
   }
-  if (sec == 60) {
-    ms = 0;
-    sec = 0;
-    min++;
+  if (seconds == 60) {
+    hundreds = 0;
+    seconds = 0;
+    minutes++;
   }
-  if (min == 60) {
-    ms = 0;
-    sec = 0;
-    min = 0;
-    hr++;
+  if (minutes == 60) {
+    hundreds = 0;
+    seconds = 0;
+    minutes = 0;
+    hours++;
   }
 
-  if (sec < 10 || sec == 0) {
-    sec = '0' + sec;
-  }
-  if (min < 10 || min == 0) {
-    min = '0' + min;
-  }
-  if (hr < 10 || hr == 0) {
-    hr = '0' + hr;
-  }
-
-  timeText.innerText = `${hr}:${min}:${sec}`;
-  miliseconds.innerText = `${ms}`;
+  timeText.innerText = `${hours < 10 ? '0' + hours : hours}:${
+    minutes < 10 ? '0' + minutes : minutes
+  }:${seconds < 10 ? '0' + seconds : seconds}`;
+  miliseconds.innerText = `${hundreds < 10 ? '0' + hundreds : hundreds}`;
 }
 
 const btns = document.getElementById('btns');
@@ -54,7 +42,7 @@ btns.addEventListener('click', e => {
     case 'start':
       startBtn.style.display = 'none';
       stopBtn.style.display = 'block';
-      time = setInterval(stopwatch, 100);
+      time = setInterval(stopwatch, 10);
       break;
     case 'stop':
       startBtn.style.display = 'block';
@@ -69,9 +57,9 @@ btns.addEventListener('click', e => {
 
 function reset(timeText, miliseconds) {
   timeText.innerText = '00:00:00';
-  miliseconds.innerText = '0';
-  ms = 0;
-  sec = 0;
-  min = 0;
-  hr = 0;
+  miliseconds.innerText = '00';
+  hundreds = 0;
+  seconds = 0;
+  minutes = 0;
+  hours = 0;
 }
